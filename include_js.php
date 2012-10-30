2798a457781089c2d50b9ecd11d5fcee0557a3e1
Rémi Chanaud <remi.chanaud@gmail.com>
2012-10-30 14:50:27 +0100

    Fichiers de base au 30/10/2012
    J'ai collé les bases du site dans le repo. C'est la version de Damien du
    jour.

---
+++ include_js.php
@@ -0,0 +1,6 @@
+<script src="js/jquery.js"></script>
+<script src="js/jquery.mousewheel.js"></script>
+<script src="js/rotate.js"></script>
+<script src="js/custom_top.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/custom_bottom.js"></script>
