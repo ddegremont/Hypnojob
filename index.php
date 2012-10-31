a5365cb7ed5d04345f291109715a1bda70a13121
Rémi Chanaud <remi.chanaud@gmail.com>
2012-10-31 10:11:31 +0100

    Commit de test
    Blabla

--- index.php
+++ index.php
@@ -37,6 +37,7 @@
 				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Test d'un commit.
 			</div>
 		</div>
 		<div class="grey bp" id="3">
