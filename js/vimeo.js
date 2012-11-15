92630d5d9c8cefe16939012d6625c8e1784b4555
Rémi Chanaud <remi.chanaud@gmail.com>
2012-11-15 16:40:17 +0100

    Patch vimeo + background

--- js/vimeo.js
+++ js/vimeo.js
@@ -5,19 +5,19 @@
 		var vimeoUsername = 'hetic';
 
 		// Get the user's videos
-		$(document).ready(function() {
-			$.getScript(apiEndpoint + vimeoUsername + '/videos.json?callback=' + videosCallback);
+		jQuery(document).ready(function() {
+			jQuery.getScript(apiEndpoint + vimeoUsername + '/videos.json?callback=' + videosCallback);
 		});
 
 		function getVideo(url) {
-			$.getScript(oEmbedEndpoint + '?url=' + url + '&width=605&height=336&color=9ECC3D&callback=' + oEmbedCallback);
+			jQuery.getScript(oEmbedEndpoint + '?url=' + url + '&width=605&height=336&color=9ECC3D&callback=' + oEmbedCallback);
 		}
 
 		function setupGallery(videos) {
 
 			// Set the user's thumbnail and the page title
-			$('#stats').prepend('<img id="portrait" src="' + videos[0].user_portrait_medium + '" />');
-			$('#stats h2').text("Vidéos de l'utilisateur " + videos[0].user_name);
+			jQuery('#stats').prepend('<img id="portrait" src="' + videos[0].user_portrait_medium + '" />');
+			jQuery('#stats h2').text("Vidéos de l'utilisateur " + videos[0].user_name);
 
 			// Load the first video
 			getVideo(videos[0].url);
@@ -26,11 +26,11 @@
 			for (var i = 0; i < videos.length; i++) {
 				var html = '<li><a href="' + videos[i].url + '"><img src="' + videos[i].thumbnail_medium + '" class="thumb" />';
 				html += '<p>' + videos[i].title + '</p></a></li>';
-				$('#thumbs ul').append(html);
+				jQuery('#thumbs ul').append(html);
 			}
 
 			// Switch to the video when a thumbnail is clicked
-			$('#thumbs a').click(function(event) {
+			jQuery('#thumbs a').click(function(event) {
 				event.preventDefault();
 				getVideo(this.href);
 				return false;
@@ -39,5 +39,5 @@
 		}
 
 		function switchVideo(video) {
-			$('#embed').html(unescape(video.html));
+			jQuery('#embed').html(unescape(video.html));
 		}
