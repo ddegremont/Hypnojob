f97ff2f995c17f5a356e096d43307583694a5d0c
Dégremont <ddegremont@gmail.com>
2012-11-23 15:57:15 +0100

    premiere maj avant la grosse avec la nav
    la nav c'est pour la prochaine

--- include_js.php
+++
@@ -1,6 +0,0 @@
-<script src="js/jquery.js"></script>
-<script src="js/jquery.mousewheel.js"></script>
-<script src="js/rotate.js"></script>
-<script src="js/custom_top.js"></script>
-<script src="js/custom.js"></script>
-<script src="js/custom_bottom.js"></script>
