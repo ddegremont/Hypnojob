d6cb18028a6c6584c9d55214d5a5d7cadf9ee467
Rémi Chanaud <remi.chanaud@gmail.com>
2012-11-28 18:45:47 +0100

    RECUPERATION DE L'INTE VIMEO
    ON TOUCHE PLUS BORDEL DE DIEU

--- js/vimeo.js
+++ js/vimeo.js
@@ -1,41 +1,41 @@
-		var apiEndpoint = 'http://vimeo.com/api/v2/';
-		var oEmbedEndpoint = 'http://vimeo.com/api/oembed.json'
-		var oEmbedCallback = 'switchVideo';
-		var videosCallback = 'setupGallery';
-		var vimeoUsername = 'hypnojob';
+var apiEndpoint = 'http://vimeo.com/api/v2/';
+var oEmbedEndpoint = 'http://vimeo.com/api/oembed.json'
+var oEmbedCallback = 'switchVideo';
+var videosCallback = 'setupGallery';
+var vimeoUsername = 'hypnojob';
 
-		// Choper les vidéos de l'utilisateur
-		jQuery(document).ready(function() {
-			jQuery.getScript(apiEndpoint + vimeoUsername + '/videos.json?callback=' + videosCallback);
-		});
+// Choper les vidéos de l'utilisateur
+jQuery(document).ready(function() {
+	jQuery.getScript(apiEndpoint + vimeoUsername + '/videos.json?callback=' + videosCallback);
+});
 
-		function getVideo(url) {
-			jQuery.getScript(oEmbedEndpoint + '?url=' + url + '&width=605&height=336&color=3296c8&callback=' + oEmbedCallback);
-		}
+function getVideo(url) {
+	jQuery.getScript(oEmbedEndpoint + '?url=' + url + '&width=605&height=336&color=3296c8&callback=' + oEmbedCallback);
+}
 
-		function setupGallery(videos) {
+function setupGallery(videos) {
 
-			// CHarger la première vidéo
-			getVideo(videos[0].url);
+	// CHarger la première vidéo
+	getVideo(videos[0].url);
 
-			// Ajoute les vidéos à la gallerie
-			for (var i = 0; i < 4; i++) {
-				var html = '<li><a href="' + videos[i].url + '"><img src="' + videos[i].thumbnail_medium + '" class="thumb" />';
-				html += '<p>' + videos[i].title + '</p></a></li>';
-				jQuery('#thumbs ul').append(html);
-			}
+	// Ajoute les vidéos à la gallerie
+	for (var i = 0; i < 4; i++) {
+		var html = '<li><a href="' + videos[i].url + '"><img src="' + videos[i].thumbnail_medium + '" class="thumb" />';
+		html += '<p>' + videos[i].title + '</p></a></li>';
+		jQuery('#thumbs ul').append(html);
+	}
 
-			// Passer la vidéo quand on clique sur une vignette
-			jQuery('#thumbs a').click(function(event) {
-				event.preventDefault();
-				getVideo(this.href);
-				return false;
-			});
+	// Passer la vidéo quand on clique sur une vignette
+	jQuery('#thumbs a').click(function(event) {
+		event.preventDefault();
+		getVideo(this.href);
+		return false;
+	});
 
-		}
+}
 
-		function switchVideo(video) {
-			var titreVideo = '<h2 id="videoTitre">'+video.title+'</h2>';
-			jQuery('#videoTitre').replaceWith(titreVideo);
-			jQuery('#embed').html(unescape(video.html));
-		}
+function switchVideo(video) {
+	var titreVideo = '<h2 id="videoTitre">'+video.title+'</h2>';
+	jQuery('#videoTitre').replaceWith(titreVideo);
+	jQuery('#embed').html(unescape(video.html));
+}
--- pages/hypnoses.php
+++ pages/hypnoses.php
@@ -1,8 +1,7 @@
 <div id="conteneur">
-	<h1>Ceci est un <strong style="color: #ff0000;">TEST</strong></h1>
 	<div id="stats">
-		<h2>Vidéos de l'utilisateur </h2>
-		<h3>Limitées à 1+3 vidéos</h3>
+		<h1>CHOISIS TON ARME</h1>
+		<h2 id="videoTitre"></h2>
 		<div style="clear: both;"></div>
 	</div>
 	<div id="wrapper">
